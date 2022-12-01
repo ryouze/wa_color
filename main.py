ca6d545ecc557516c7529392cb2e8c2ac0ef031c
ryouze <98982999+ryouze@users.noreply.github.com>
2022-12-01 13:18:27 +0100

    use ArgumentParser instead of sys.argv

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ Main entry: setup rotating logger and run app.
 import logging
 import sys
 from logging.handlers import RotatingFileHandler
+from argparse import ArgumentParser
 
 from wa_color.main import App, Debug
 
@@ -33,40 +34,58 @@ def main() -> None:
         ],
         level=logging.INFO,
     )
-    # check for cli arguments (e.g., 'main.py --mail')
+    # process CLI arguments (e.g., 'main.py --mail')
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="flag: enable verbose, debug-level logging",
+        action="store_true",  # doesn't take value, returns true if provided, otherwise false
+    )
+    parser.add_argument(
+        "--reset",
+        help="flag: destroy all data, equivalent to running the program for the first time",
+        action="store_true",  # doesn't take value, returns true if provided, otherwise false
+    )
+    parser.add_argument(
+        "--html",
+        help="flag: create html table based on the lesson plan, then quit",
+        action="store_true",  # doesn't take value, returns true if provided, otherwise false
+    )
+    parser.add_argument(
+        "--mail",
+        help="flag: send debug e-mail to all recipients, then quit",
+        action="store_true",  # doesn't take value, returns true if provided, otherwise false
+    )
+    args = parser.parse_args()
     reset: bool = False
-    if len(sys.argv) > 1:
-        first_arg: str = sys.argv[1]
+    if args.verbose:
+        logging.info("found arg 'verbose' - enable verbose, debug-level logging")
         logging.getLogger().setLevel(logging.DEBUG)
-        if first_arg == "--verbose":
-            # run webscraper normally
-            logging.info("found arg: '--verbose', using verbose DEBUG-level logging")
-        elif first_arg == "--reset":
-            # run webscraper normally, but re-create all directories from scratch first
-            # this also sets 'loop_wait_time' to 0, so it will quit after first loop
-            logging.info(
-                "found arg: '--reset', re-creating all directories from scratch"
-            )
-            reset = True
-        elif first_arg == "--mail":
-            # send debug e-mail, then quit without running webscraper
-            logging.info("found arg: '--mail', sending debug e-mail now")
-            debug = Debug()
-            r = debug.mail()
-            logging.info(f"mail success status: {r}")
-            return None
-        elif first_arg == "--html":
-            # send create html file from lesson plan, then quit without running webscraper
-            logging.info("found arg: '--html', creating html file now")
-            debug = Debug()
-            r = debug.html()
-            logging.info(f"html success status: {r}")
-            return None
-        else:
-            logging.error(f"unknown cli argument: '{sys.argv[1]}', quitting")
-            return None
-    else:
-        logging.debug("ok: no cli argument provided")
+    if args.reset:
+        # run webscraper normally, but re-create all directories from scratch first
+        # this also sets 'loop_wait_time' to 0, so it will quit after first loop
+        logging.info(
+            "found arg 'reset' - destroy all data, equivalent to running the program for the first time"
+        )
+        reset = True
+    if args.html:
+        # create html file from lesson plan, then quit without running webscraper
+        logging.info(
+            "found arg 'html' - create html table based on the lesson plan, then quit"
+        )
+        debug = Debug()
+        r = debug.html()
+        return None
+    if args.mail:
+        # send debug e-mail, then quit without running webscraper
+        logging.info(
+            "found arg 'mail' - send debug e-mail to all recipients, then quit"
+        )
+        debug = Debug()
+        r = debug.mail()
+        logging.info(f"mail success status: {r}")
+        return None
     # run webscraper as an infinite loop (unless 'loop_wait_time' is set to 0)
     app = App(reset=reset)
     app.run()
